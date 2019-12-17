36b31260ca3bf8535587eeb4c143982c1be976de
wjtf <2644970580@qq.com>
2019-12-17 18:09:26 +0800

    十次方项目第二天代码

---
+++ tensquare_article/src/main/java/com/tensquare/article/service/CommentService.java
@@ -0,0 +1,26 @@
+package com.tensquare.article.service;
+
+import com.tensquare.article.pojo.Comment;
+import com.tensquare.article.repository.CommentReopsitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    private CommentReopsitory commentReopsitory;
+
+    public List<Comment> findAll() {
+        List<Comment> list = commentReopsitory.findAll();
+        return list;
+
+    }
+
+    public Comment findById(String id) {
+        Comment comment = commentReopsitory.findById(id).get();
+        return comment;
+    }
+}
